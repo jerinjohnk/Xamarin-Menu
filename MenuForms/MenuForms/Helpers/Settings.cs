003ef56570f82a279b58b23e5eadbeedcbed133d
DESKTOP\jerin <jerin.john@girnarsoft.com>
2017-06-23 19:45:39 +0530

    Reviving the Repo

--- MenuForms/MenuForms/Helpers/Settings.cs
+++ MenuForms/MenuForms/Helpers/Settings.cs
@@ -4,55 +4,40 @@ using Plugin.Settings.Abstractions;
 
 namespace MenuForms.Helpers
 {
-    /// <summary>
-    /// This is the Settings static class that can be used in your Core solution or in any
-    /// of your client applications. All settings are laid out the same exact way with getters
-    /// and setters. 
-    /// </summary>
-    public static class Settings
+  /// <summary>
+  /// This is the Settings static class that can be used in your Core solution or in any
+  /// of your client applications. All settings are laid out the same exact way with getters
+  /// and setters. 
+  /// </summary>
+  public static class Settings
+  {
+    private static ISettings AppSettings
     {
-        private static ISettings AppSettings
-        {
-            get
-            {
-                return CrossSettings.Current;
-            }
-        }
-
-        #region Setting Constants
-
-        private const string SettingsKey = "settings_key";
-        private static readonly string SettingsDefault = string.Empty;
-
-        private const string AccessTokenKey = "access_token_key";
-        private static readonly string AccessTokenDefault = string.Empty;
-
-        #endregion
-
-
-        public static string GeneralSettings
-        {
-            get
-            {
-                return AppSettings.GetValueOrDefault<string>(SettingsKey, SettingsDefault);
-            }
-            set
-            {
-                AppSettings.AddOrUpdateValue<string>(SettingsKey, value);
-            }
-        }
-
-        public static string AccessToken
-        {
-            get
-            {
-                return AppSettings.GetValueOrDefault(AccessTokenKey, AccessTokenDefault);
-            }
-            set
-            {
-                AppSettings.AddOrUpdateValue(AccessTokenKey, value);
-            }
-        }
-
+      get
+      {
+        return CrossSettings.Current;
+      }
     }
+
+    #region Setting Constants
+
+    private const string SettingsKey = "settings_key";
+    private static readonly string SettingsDefault = string.Empty;
+
+    #endregion
+
+
+    public static string GeneralSettings
+    {
+      get
+      {
+        return AppSettings.GetValueOrDefault<string>(SettingsKey, SettingsDefault);
+      }
+      set
+      {
+        AppSettings.AddOrUpdateValue<string>(SettingsKey, value);
+      }
+    }
+
+  }
 }
